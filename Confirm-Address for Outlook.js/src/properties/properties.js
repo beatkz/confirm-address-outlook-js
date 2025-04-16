4d4ae848bac5b1ca4a8792d76b6ee7c807447b77
Keiji Momose <keiji.momose@soliton.co.jp>
2025-04-16 16:39:38 +0900

    webpack.config.js: 頻繁に変更する項目と、そうでない項目を区別するためのコードの整頓を行った。 タスクペインページの workbed を properties に変更。

---
+++ Confirm-Address for Outlook.js/src/properties/properties.js
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global document, Office */
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Outlook) {
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("run").onclick = run;
+    run();
+  }
+});
+
+export async function run() {
+/**
+* Insert your Outlook code here
+*/
+
+const item = Office.context.mailbox.item;
+const properties = document.getElementById("properties");
+properties.innerHTML = "";
+
+// 宛先
+const toResult = await new Promise((resolve) => item.to.getAsync(resolve));
+const toReci = toResult.value.map((r) => r.emailAddress).join("<br/>") || "なし";
+addProperty(properties, "送信先 [To]", toReci);
+
+// Cc
+const ccResult = await new Promise((resolve) => item.cc.getAsync(resolve));
+const ccReci = ccResult.value.map((r) => r.emailAddress).join("<br/>") || "なし";
+addProperty(properties, "送信先 [Cc]", ccReci);
+
+// Bcc
+const bccResult = await new Promise((resolve) => item.bcc.getAsync(resolve));
+const bccReci = bccResult.value.map((r) => r.emailAddress).join("<br/>") || "なし";
+addProperty(properties, "送信先 [Bcc]", bccReci);
+
+// 本文冒頭
+const bodyResult = await new Promise((resolve) => item.body.getAsync("text", resolve));
+const lines = 8;
+const body = bodyResult.value.split("\n").slice(0, lines).join("<br/>") || "なし";
+addProperty(properties, `本文冒頭 ${lines}行`, body);
+
+// 添付ファイル名
+const attResult = await new Promise((resolve) => item.getAttachmentsAsync(resolve));
+const attNames = attResult.value.map((att) => att.name).join("<br/>") || "なし";
+addProperty(properties, "添付ファイル名", attNames);
+}
+
+function addProperty(list, label, value) {
+ const li = document.createElement("li");
+ li.innerHTML = `<strong>${label}:</strong><br/>${value}`;
+ list.appendChild(li);
+}
--- Confirm-Address for Outlook.js/webpack.config.js
+++ Confirm-Address for Outlook.js/webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "https://github.com/beatkz/confirm-address-outlook-js/"; 
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
@@ -13,15 +13,54 @@ async function getHttpsOptions() {
 }
 
 module.exports = async (env, options) => {
-  const dev = options.mode === "development";
   const config = {
-    devtool: "source-map",
     entry: {
       polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
+      properties: ["./src/properties/properties.js", "./src/properties/properties.html"],
       taskpane: ["./src/taskpane/taskpane.js", "./src/taskpane/taskpane.html"],
-      workbed: ["./src/workbed/workbed.js", "./src/workbed/workbed.html"],
       commands: "./src/commands/commands.js",
     },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: "properties.html",
+        template: "./src/properties/properties.html",
+        chunks: ["polyfill", "properties"],
+      }),
+      new HtmlWebpackPlugin({
+        filename: "taskpane.html",
+        template: "./src/taskpane/taskpane.html",
+        chunks: ["polyfill", "taskpane"],
+      }),
+      new HtmlWebpackPlugin({
+        filename: "commands.html",
+        template: "./src/commands/commands.html",
+        chunks: ["polyfill", "commands"],
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: "assets/*",
+            to: "assets/[name][ext][query]",
+          },
+          {
+            from: "manifest*.json",
+            to: "[name]" + "[ext]",
+            transform(content) {
+              if (dev) {
+                return content;
+              } else {
+                return content.toString().replace(new RegExp(urlDev, "g"), urlProd);
+              }
+            },
+          },
+          {
+            from: "./src/launchevent/launchevent.js",
+            to: "launchevent.js",
+          },
+        ],
+      }),
+    ],
+    devtool: "source-map",
     output: {
       clean: true,
     },
@@ -51,46 +90,6 @@ module.exports = async (env, options) => {
         },
       ],
     },
-    plugins: [
-      new HtmlWebpackPlugin({
-        filename: "taskpane.html",
-        template: "./src/taskpane/taskpane.html",
-        chunks: ["polyfill", "taskpane"],
-      }),
-      new HtmlWebpackPlugin({
-        filename: "workbed.html",
-        template: "./src/workbed/workbed.html",
-        chunks: ["polyfill", "workbed"],
-      }),
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: "assets/*",
-            to: "assets/[name][ext][query]",
-          },
-          {
-            from: "manifest*.json",
-            to: "[name]" + "[ext]",
-            transform(content) {
-              if (dev) {
-                return content;
-              } else {
-                return content.toString().replace(new RegExp(urlDev, "g"), urlProd);
-              }
-            },
-          },
-          {
-            from: "./src/launchevent/launchevent.js",
-            to: "launchevent.js",
-          },
-        ],
-      }),
-      new HtmlWebpackPlugin({
-        filename: "commands.html",
-        template: "./src/commands/commands.html",
-        chunks: ["polyfill", "commands"],
-      }),
-    ],
     devServer: {
       headers: {
         "Access-Control-Allow-Origin": "*",
@@ -102,6 +101,6 @@ module.exports = async (env, options) => {
       port: process.env.npm_package_config_dev_server_port || 3000,
     },
   };
-
+  const dev = options.mode === "development";
   return config;
 };
