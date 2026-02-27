b249704c7f5e31ba042837f771ad0de16fe67758
Keiji Momose <keiji.momose@gmail.com>
2026-02-27 16:51:13 +0900

    リリースブランチビルド時のエラーを修正。

--- Confirm-Address for Outlook.js/webpack.config.js
+++ Confirm-Address for Outlook.js/webpack.config.js
@@ -64,7 +64,7 @@ module.exports = async (env, options) => {
               if (dev) {
                 return content;
               } else {
-                return content.toString().replace(new RegExp(urlDev, "g"), urlProd);
+                return content.toString().replace(new RegExp(urlDev, "g"), urlStable);
               }
             },
           },
