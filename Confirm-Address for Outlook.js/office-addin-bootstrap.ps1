84c38c40372a0d082eeb5e708b628b273719e6fa
Keiji Momose <keiji.momose@gmail.com>
2026-06-16 10:33:39 +0900

    office-addin-bootstrap powershell script added.

--- Confirm-Address for Outlook.js/office-addin-bootstrap.ps1
+++ Confirm-Address for Outlook.js/office-addin-bootstrap.ps1
@@ -5,7 +5,6 @@
 
 Param(
     [switch]$debug,
-    [switch]$deploy,
     [switch]$halt
     )
 
@@ -16,27 +15,8 @@ if ($debug) {
     & npm run build:dev
     & npm run start
 }
-elseif ($deploy) {
-    # 現在のGitブランチを取得
-    $branch = git branch --show-current
-
-    # ローカルモード（devブランチ）の判定
-    $localMode = ($branch -eq "dev")
-
-    if ($localMode) {
-        Read-Host -Prompt "You are currently on the 'dev' branch. After committing your changes, hit any key to continue."
-    }
-
-    & git checkout main
-    & "./deployghio.ps1"
-    if ($LASTEXITCODE -ne 0) {
-        Write-Error "Deployment failed. Please check the errors above."
-        exit $LASTEXITCODE
-    }
-    & git checkout dev
-}
 elseif ($halt) {
     & npm run stop
 } else {
-    Write-Host "office-addin-bootstrap: Please specify either -debug, -deploy, or -halt." -ForegroundColor Yellow
+    Write-Host "office-addin-bootstrap: Please specify either -debug, or -halt." -ForegroundColor Yellow
 }
